5c0f0d32a3c0654d3353a02d659f216f7679e23d
JCGJ94 <jcgonj1994@gmail.com>
2025-10-22 21:59:27 +0100

    fit: bug css styls

--- src/front/components/Landing.jsx
+++ src/front/components/Landing.jsx
@@ -1,4 +1,9 @@
 const Landing = () => {
+  
+
+
+
+
   return (
     <>
       <div className="backgroundimg">
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -3,6 +3,8 @@ import Statistics from "../components/Statistics.jsx";
 import Landing from "../components/Landing.jsx";
 import WhoWeAre from "../components/WhoWeAre.jsx";
 import LandingLogged from "../components/LandingLogged.jsx";
+
+
 export const Home = () => {
 
 
